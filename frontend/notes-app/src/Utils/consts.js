81cfbd65628772c1ea2ace20f55b3cdb88b54ae5
IBadella <ignacio.badella@ayi.group>
2024-01-12 17:46:38 -0300

    change url to backend

--- frontend/notes-app/src/Utils/consts.js
+++ frontend/notes-app/src/Utils/consts.js
@@ -1,8 +1,8 @@
 // URLs
-export const URL_NOTES = 'http://localhost:8080/wsnotes/notes'
+/* export const URL_NOTES = 'http://localhost:8080/wsnotes/notes'
 export const URL_UPDATE_NOTES = 'http://localhost:8080/wsnotes//notes/update'
-export const URL_CREATE_NOTES = 'http://localhost:8080/wsnotes//notes/create'
+export const URL_CREATE_NOTES = 'http://localhost:8080/wsnotes//notes/create' */
 
-/* export const URL_NOTES = 'https://notes-challenge-production.up.railway.app/wsnotes/notes'
+export const URL_NOTES = 'https://notes-challenge-production.up.railway.app/wsnotes/notes'
 export const URL_UPDATE_NOTES = 'https://notes-challenge-production.up.railway.app/wsnotes/notes/update'
-export const URL_CREATE_NOTES = 'https://notes-challenge-production.up.railway.app/wsnotes/notes/create' */
+export const URL_CREATE_NOTES = 'https://notes-challenge-production.up.railway.app/wsnotes/notes/create'
